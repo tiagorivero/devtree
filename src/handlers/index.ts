89735f1ab13e95398dcaa9ac4198e95fe44f7a8b
Tiago Rivero <tiagorivero100@gmail.com>
2026-08-11 15:47:24 -0300

    Hashing passwords

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,8 +1,9 @@
 import User from "../models/Users"
 import type { Request, Response } from 'express'
+import { hashPassword } from "../utils/auth"
 
 export const createAccount = async (req : Request, res: Response) => {
-    const { email } = req.body
+    const { email, password } = req.body
 
     const userExists = await User.findOne({ email })
     if(userExists){
@@ -11,8 +12,7 @@ export const createAccount = async (req : Request, res: Response) => {
     }
 
     const user = new User(req.body)
-
+    user.password = await hashPassword(password)
     await user.save()
-
-    res.send({msg: 'Registro Creado Correctamente'})
+    res.status(201).send({msg: 'Registro Creado Correctamente'})
 }
