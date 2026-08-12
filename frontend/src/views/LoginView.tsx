19bdee20d8c5c46e44525ff518b137b3349ab47a
Tiago Rivero <tiagorivero100@gmail.com>
2026-08-12 00:26:43 -0300

    Creating the frontend for the login and register section

--- frontend/src/views/LoginView.tsx
+++ frontend/src/views/LoginView.tsx
@@ -3,10 +3,11 @@ import { Link } from 'react-router-dom'
 export default function LoginView() {
   return (
     <>
-        <div>LoginView</div>
+        <h1 className='text-4xl text-white font-bold'>Iniciar Sesion</h1>
 
-        <nav>
-            <Link to="/auth/register">
+        <nav className='mt-10'>
+            <Link className='text-center text-white text-lg block'
+                to="/auth/register">
                 No tienes cuenta? Crea una aqui
             </Link>
         </nav>
--- frontend/src/views/RegisterView.tsx
+++ frontend/src/views/RegisterView.tsx
@@ -1,12 +1,85 @@
 import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import ErrorMessage from '../components/ErrorMessage'
 
 export default function RegisterView() {
+  const initialValues = {
+    name: '',
+    email: '',
+    handle: '',
+    password: '',
+    password_confirmation: ''
+  }
+
+  const { register, watch, handleSubmit, formState: { errors } } = useForm({defaultValues : initialValues})
+
+  console.log(errors)
+
+  const handleRegister = () => {
+    console.log('Desde HandleRegister')
+  }
+
   return (
     <>
-      <div>Register</div>
+      <h1 className='text-4xl text-white font-bold'>Crear Cuenta</h1>
 
-      <nav>
-            <Link to="/auth/login">
+      <form onSubmit={handleSubmit(handleRegister)} className="bg-white px-5 py-8 rounded-lg space-y-10 mt-10">
+          <div className="grid grid-cols-1 space-y-3">
+              <label htmlFor="name" className="text-2xl text-slate-500">Nombre</label>
+              <input id="name" type="text" placeholder="Tu Nombre" className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400" 
+              {...register('name',{
+                required: "El nombre es obligatorio"
+              })}/>
+              {errors.name && <ErrorMessage>{errors.name.message}</ErrorMessage> }
+          </div>
+
+          <div className="grid grid-cols-1 space-y-3">
+              <label htmlFor="email" className="text-2xl text-slate-500">E-mail</label>
+              <input id="email" type="email" placeholder="Email de Registro" className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400" 
+              {...register('email',{
+                required: "El email es obligatorio"
+              })}
+              />
+              {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage> }
+          </div>
+          
+          <div className="grid grid-cols-1 space-y-3">
+              <label htmlFor="handle" className="text-2xl text-slate-500">Handle</label>
+              <input id="handle" type="text" placeholder="Nombre de usuario: sin espacios" className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400" 
+              {...register('handle',{
+                required: "El Handle es obligatorio"
+              })}
+              />
+              {errors.handle && <ErrorMessage>{errors.handle.message}</ErrorMessage> }
+          </div>
+
+          <div className="grid grid-cols-1 space-y-3">
+              <label htmlFor="password" className="text-2xl text-slate-500">Password</label>
+              <input id="password" type="password" placeholder="Password de Registro" className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400" 
+              {...register('password',{
+                required: "El Password es obligatorio"
+              })}
+              />
+              {errors.password && <ErrorMessage>{errors.password.message}</ErrorMessage> }
+          </div>
+
+          <div className="grid grid-cols-1 space-y-3">
+              <label htmlFor="password_confirmation" className="text-2xl text-slate-500">Repetir Password</label>
+              <input id="password_confirmation" type="password" placeholder="Repetir Password" className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400" 
+              {...register('password_confirmation',{
+                required: "Repetir password es obligatorio"
+              })}
+              />
+              {errors.password_confirmation && <ErrorMessage>{errors.password_confirmation.message}</ErrorMessage> }
+          </div>
+
+          <input type="submit" className="bg-cyan-400 p-3 text-lg w-full uppercase text-slate-600 rounded-lg font-bold cursor-pointer" value='Crear Cuenta' />  
+      </form>
+
+      <nav className='mt-10'>
+            <Link 
+              className='text-center text-white text-lg block'
+            to="/auth/login">
                 Ya tienes cuenta? Inicia Sesion
             </Link>
         </nav>
