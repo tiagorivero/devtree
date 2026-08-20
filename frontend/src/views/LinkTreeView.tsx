8777fff98d2d15665c315d281f89cd21b2cae215
Tiago Rivero <tiagorivero100@gmail.com>
2026-08-19 23:44:48 -0300

    Adding the links section to the frontend

--- frontend/src/views/LinkTreeView.tsx
+++ frontend/src/views/LinkTreeView.tsx
@@ -1,5 +1,44 @@
+import { useState } from "react"
+import { social } from "../data/social"
+import DevTreeInput from "../components/DevTreeInput"
+import { isValidUrl } from "../utils"
+import { toast } from "sonner"
+
 export default function LinkTreeView() {
+  const [devTreeLinks, setDevTreeLinks] = useState(social)
+
+  const handleUrlChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+    const updatedLinks = devTreeLinks.map(link => link.name === e.target.name ? {...link, url: e.target.value} : link)
+    setDevTreeLinks(updatedLinks)
+  }
+
+  const handleEnableLink = (socialNetwork: string) => {
+    const updatedLinks = devTreeLinks.map(link => {
+      if(link.name === socialNetwork) {
+        if(isValidUrl(link.url)) {
+          return {...link,enabled: !link.enabled}
+        } else {
+          toast.error('URL no valida')
+        }
+      } 
+      return link
+    })
+    setDevTreeLinks(updatedLinks)
+  }
+
   return (
-    <div>LinkTreeView</div>
+    <>
+      <div className="space-y-5">
+        {devTreeLinks.map(item => (
+          <DevTreeInput 
+            key={item.name} 
+            item={item}  
+            handleUrlChange={handleUrlChange}
+            handleEnableLink={handleEnableLink}
+          />
+        ))}
+      </div>
+    </>
+    
   )
 }
